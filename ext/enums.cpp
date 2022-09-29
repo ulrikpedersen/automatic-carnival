3e4d0f8ee07c69825d3408463aab61ac1e69f265
Matveyev <yury.matveev@desy.de>
2022-09-29 13:38:31 +0200

    fixed tango import in enums.cpp (merge mistake)

--- ext/enums.cpp
+++ ext/enums.cpp
@@ -10,7 +10,7 @@
 ******************************************************************************/
 
 #include "precompiled_header.hpp"
-#include <tango.h>
+#include <tango/tango.h>
 
 #include <boost/utility/enable_if.hpp>
 
