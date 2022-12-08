687beca8891a7af0a562c69c9eefd3aafdd75b67
Ulrik Pedersen <ukp@observatorysciences.co.uk>
2022-12-08 13:16:51 +0000

    Removing 'u' string prefix
    Ref #478 - python3 strings are unicode

--- tests/test_server.py
+++ tests/test_server.py
@@ -853,7 +853,7 @@ def test_device_get_attr_config(server_green_mode):
             # testing that call to get_attribute_config for all types of
             # input arguments gives same result and doesn't raise an exception
             ac1 = self.get_attribute_config(b"attr_config_ok")
-            ac2 = self.get_attribute_config(u"attr_config_ok")
+            ac2 = self.get_attribute_config("attr_config_ok")
             ac3 = self.get_attribute_config(["attr_config_ok"])
             return repr(ac1) == repr(ac2) == repr(ac3)
 
