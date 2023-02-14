41bd20147b9a9b6b6492e90d26bbaa3b37929cca
Anton Joubert <1346825-ajoubertza@users.noreply.gitlab.com>
2023-02-14 10:24:57 +0100

    Change Device metaclass usage to Python 3 syntax
    Creating `Device` by instantiating with `DeviceMeta`
    and using a type hint is not sufficient for autocompletion
    in IDEs like PyCharm.  The methods are shown, but the
    declaration and docstring cannot be shown for inherited
    methods.
    
    Changing `Device` to use the Python 3 metaclass syntax
    fixes the type autocompletion problem and keeps the same
    behaviour.  We have a unit test for device inheritance.

--- tango/server.py
+++ tango/server.py
@@ -1728,12 +1728,12 @@ def server_run(classes, args=None, msg_stream=sys.stdout,
                green_mode=green_mode)
 
 
-# Instantiate DeviceMeta using BaseDevice
-Device: BaseDevice = DeviceMeta("Device", (BaseDevice,), {'__doc__': """\
+class Device(BaseDevice, metaclass=DeviceMeta):
+    """
     Device class for the high-level API.
 
     All device-specific classes should inherit from this class.
-    """})
+    """
 
 # Avoid circular imports
 from .tango_object import Server  # noqa: E402
